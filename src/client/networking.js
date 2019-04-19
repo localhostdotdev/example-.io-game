71da75413c8dfda8b27d5ebe952c83a9ed4312e3
Victor Zhou <vzhou842@gmail.com>
2019-04-19 18:17:30 -0400

    Setup HTTPS and deploy script

--- src/client/networking.js
+++ src/client/networking.js
@@ -6,7 +6,10 @@ import { processGameUpdate } from './state';
 
 const Constants = require('../shared/constants');
 
-const socket = io(`ws://${window.location.host}`, { reconnection: false });
+const socket = io(
+  `${window.location.host !== 'localhost' ? 'wss' : 'ws'}://${window.location.host}`,
+  { reconnection: false },
+);
 const connectedPromise = new Promise(resolve => {
   socket.on('connect', () => {
     console.log('Connected to server!');
@@ -14,7 +17,7 @@ const connectedPromise = new Promise(resolve => {
   });
 });
 
-export const connect = onGameOver => (
+export const connect = onGameOver =>
   connectedPromise.then(() => {
     // Register callbacks
     socket.on(Constants.MSG_TYPES.GAME_UPDATE, processGameUpdate);
@@ -26,8 +29,7 @@ export const connect = onGameOver => (
         window.location.reload();
       };
     });
-  })
-);
+  });
 
 export const play = username => {
   socket.emit(Constants.MSG_TYPES.JOIN_GAME, username);
